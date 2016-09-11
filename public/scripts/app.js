8035b94feb97a404c87e56e2d6b98627cb527f97
wnamen <williamjosephnamen@gmail.com>
2016-09-11 16:46:06 -0700

    added route to server for ingredients

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,37 +2,26 @@ console.log("sanity check is working!!!!");
 
 var recipeHtml;
 var recipeTemplate;
-var ingredientHtml;
-var ingredientTemplate;
-var ingredientList = [];
-
+var data;
 
 $(document).ready(function() {
 
-  // recipeHtml = $('#recipe-template').html();
-  // recipeTemplate = Handlebars.compile(recipeHtml);
-
-  // ingredientHtml = $('#ingredient-template').html();
-  // ingredientTemplate = Handlebars.compile(ingredientHtml);
+  recipeHtml = $('#recipe-template').html();
+  recipeTemplate = Handlebars.compile(recipeHtml);
 
   $(".input-append").on("submit", function(e){
   	e.preventDefault();
 
     var ingredientData = $(this).serialize();
     var index = ingredientData.indexOf("=");
-    var data = ingredientData.slice(index + 1);
-    console.log(data);
-    // console.log('ingredientData', ingredientData);
+    data = ingredientData.slice(index + 1);
 
-    // $.post('/api/ingredients', ingredientData, function(ingredient) {
-    //   console.log('ingredient after POST', ingredient);
-    //   renderIngredient(ingredient);  //render the server's response
-    // });
-    // renderIngredient(ingredientData);
-	$('.ingredients').append("<div class='items'><div class='item'>" + data + "</div><div class='delete'><button>X</button></div>");
-	// $('.ingredients').append( );
+    $.post('/api/ingredients', ingredientData, function(ingredient) {
+      renderIngredient(ingredient);  //render the server's response
+    });
 
   	$(this).trigger("reset");
+  });
 
 
   // make a get request for top 30 recipes
@@ -58,21 +47,6 @@ $(document).ready(function() {
 
       $(this).trigger("reset");
     });
-
-
-  // $.get('/api/recipes').success(function(recipes) {
-  //   recipes.forEach(function(recipe) {
-  // 		// console.log('in here');
-  //     renderRecipe(recipe);
-  //   });
-  // });
-
-  // $.get('/api/ingredients').success(function(ingredients){
-  // 	ingredients.forEach(function(ingredient) {
-  // 		renderIngredient(ingredient);
-  // 	});
-  // });
-
 });
 
 function handleRecipes(json){
@@ -84,8 +58,7 @@ function handleRecipes(json){
 
 function renderIngredient(ingredient) {
   // console.log(ingredient);
-  var html = ingredientTemplate(ingredient);
-  $('#right-search').prepend(html);
+  $('#right-search').append("<div class='items'><div class='item'>" + data + "</div><div class='delete'><button>X</button></div>");
 }
 
 function renderRecipe(recipe) {
