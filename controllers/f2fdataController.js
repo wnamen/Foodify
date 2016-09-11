5ded9f7a05e382cb760746ac1f4ab1c6125ccbd8
wnamen <williamjosephnamen@gmail.com>
2016-09-10 20:09:43 -0700

    added controller for query search

--- controllers/f2fdataController.js
+++ controllers/f2fdataController.js
@@ -1,8 +1,5 @@
 var unirest = require('unirest');
 
-
-// var data = server.data;
-
 function index(req, res) {
   unirest.get('http://food2fork.com/api/search?key=1b70f7c3dffb5c592b9bb7ae1accc823')
   .header("Accept", "JSON")
@@ -16,7 +13,14 @@ function create(req, res){
 }
 
 function show(req, res){
+  var data = req.query.q;
 
+  console.log(data);
+  unirest.get('http://food2fork.com/api/search?key=1b70f7c3dffb5c592b9bb7ae1accc823&q=' + data)
+  .header("Accept", "JSON")
+  .end(function (result) {
+    res.json(JSON.parse(result.body));
+  });
 }
 
 function destroy(req, res){
