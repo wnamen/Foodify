8b6c26dcb0d891408fb3752b7c317c1a07da8913
wnamen <williamjosephnamen@gmail.com>
2016-09-10 20:07:35 -0700

    add get request for query

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,5 @@
 console.log("sanity check is working!!!!");
 
-var recipeUrl = "http://food2fork.com/api/get?key=1b70f7c3dffb5c592b9bb7ae1accc823";
-
 var recipeHtml;
 var recipeTemplate;
 var ingredientHtml;
@@ -18,11 +16,27 @@ $(document).ready(function() {
 
   // make a get request for top 30 recipes
   $.ajax({
-  method: "get",
-  url: '/api/f2fdata',
-  dataType: 'JSON',
-  success: onSuccess
+    method: "GET",
+    url: '/api/f2fdata',
+    dataType: 'JSON',
+    success: handleRecipes
   });
+
+  $('#search-form').on('submit', function(e) {
+      e.preventDefault();
+
+      $("#recipes").empty();
+
+      $.ajax({
+        method: "GET",
+        url: '/api/f2fdata/query',
+        dataType: 'JSON',
+        data: $(this).serialize(),
+        success: handleRecipes
+      });
+
+      $(this).trigger("reset");
+    });
 
   // $.get('/api/recipes').success(function(recipes) {
   //   recipes.forEach(function(recipe) {
@@ -39,9 +53,8 @@ $(document).ready(function() {
 
 });
 
-function onSuccess(json){
+function handleRecipes(json){
   var recipes = json.recipes;
-  console.log(recipes);
   recipes.forEach(function(recipe) {
     renderRecipe(recipe);
   });
