498fe79d6bde14ddd43692141afe574c5f205d76
wnamen <williamjosephnamen@gmail.com>
2016-09-11 21:12:00 -0700

    added a search request to api

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,24 +4,12 @@ var recipeHtml;
 var recipeTemplate;
 var data;
 
+var ingredientList = "q=";
+
 $(document).ready(function() {
 
   recipeHtml = $('#recipe-template').html();
   recipeTemplate = Handlebars.compile(recipeHtml);
-
-  $(".search-form").on("submit", function(e){
-  	e.preventDefault();
-
-    var ingredientData = $(this).serialize();
-    var index = ingredientData.indexOf("=");
-    data = ingredientData.slice(index + 1);
-
-    $.post('/api/ingredients', ingredientData, function(ingredient) {
-      renderIngredient(ingredient);  //render the server's response
-    });
-
-  	$(this).trigger("reset");
-  });
 
   // make a get request for top 30 recipes
   $.ajax({
@@ -31,38 +19,61 @@ $(document).ready(function() {
     success: handleRecipes
   });
 
+  //renders ingredients and posts them to the DB
+  $("#render-form").on("submit", function(e){
+  	e.preventDefault();
 
+    var ingredientData = $(this).serialize();
+    var index = ingredientData.indexOf("=");
+    data = ingredientData.slice(index + 1);
 
-  	// $('.item').on('click', function(e) {
-  	// 	var test = $(this).attr('id');
-  	// 	console.log(test);
-  	// });
+    ingredientList += data + "+";
+    console.log(ingredientList);
 
+    $.post('/api/ingredients', ingredientData, function(ingredient) {
+      renderIngredient(ingredient);  //render the server's response
+    });
 
+  	$(this).trigger("reset");
+  });
+
+  //renders ingredient in search form
 	$('.ingredients').on('click', '.delete', function(e) {
-		console.log("hello");
 		e.preventDefault();
 		var idName = $(this).attr('id');
-		console.log(idName);
 		$("#" + idName).remove();
 	});
+
+  //clears the ingredients from the search form
 	$('#clear-form').on('click', function(e) {
 		e.preventDefault();
+    ingredientList = "q=";
 		$('.ingredients').empty();
 	})
 
-  // make a get request for top 30 recipes
-  // $.ajax({
-  // method: "get",
-  // url: 'http://food2fork.com/api/search?key=1b70f7c3dffb5c592b9bb7ae1accc823',
-  // dataType: 'JSON',
-  // success: onSuccess
-  // });
+  //searches api for ingrediet list query
+  $('#form-submit').on('click', function(e) {
+    e.preventDefault();
+    $("#recipes").empty();
 
+    $.ajax({
+      method: "GET",
+      url: '/api/f2fdata/query',
+      dataType: 'JSON',
+      data: ingredientList,
+      success: handleRecipes
+    });
+
+    $(this).trigger("reset");
+  });
+
+
+  //searches api for query using search bar
   $('#search-form').on('submit', function(e) {
       e.preventDefault();
-
       $("#recipes").empty();
+
+      console.log($(this).serialize());
 
       $.ajax({
         method: "GET",
