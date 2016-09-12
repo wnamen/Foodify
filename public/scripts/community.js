8e8346634dcb1c99fdff1ff3a19e44cb24e3de4a
ashleyvernon <avernon526@gmail.com>
2016-09-12 14:29:22 -0700

    Post request for creating new recipe

--- public/scripts/community.js
+++ public/scripts/community.js
@@ -2,18 +2,42 @@ console.log("sanity check is working!!!!");
 
 var recipeHtml;
 var recipeTemplate;
-var data;
 
-var ingredientList = "q=";
 
 $(document).ready(function() {
 
   recipeHtml = $('#recipe-template').html();
   recipeTemplate = Handlebars.compile(recipeHtml);
+
+  $.ajax({
+  	method: 'GET',
+  	url: '/api/recipes',
+  	data: 'json',
+  	success: handleRecipes
+  });
+
+  $('#recipe-form').submit(handleFormSubmit);
+
+
 });
 
+function handleFormSubmit(e){
+	// e.preventDefault();
+
+	$.ajax({
+		method: 'POST',
+		url: '/api/recipes',
+		dataType: 'json',
+		data: $('#recipe-form').serializeArray(),
+		success: handleRecipes
+	});
+
+	$(this).trigger('reset');
+
+}
+
 function handleRecipes(json){
-  var recipes = json.recipes;
+  var recipes = json;
   recipes.forEach(function(recipe) {
     renderRecipe(recipe);
   });
