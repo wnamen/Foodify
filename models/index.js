7ed66f83bac5f78f493462723f3843045e9d93d3
wnamen <williamjosephnamen@gmail.com>
2016-09-12 12:15:26 -0700

    updated uri

--- models/index.js
+++ models/index.js
@@ -1,8 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/foodify");
-
-var mongodbUri = "//heroku_nz7cmb4h:2ffgddais5iuiln8h9iqrmndcb@ds029436.mlab.com:29436/heroku_nz7cmb4h";
+var MONGODB_URI = "//heroku_nz7cmb4h:2ffgddais5iuiln8h9iqrmndcb@ds029436.mlab.com:29436/heroku_nz7cmb4h";
 var db = mongoose.connection;
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/foodify");
 
 var Recipe = require('./recipes');
 var Ingredient = require('./ingredients');
