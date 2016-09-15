c564342c0abf3319e8da435104a42c133c40d0e3
ashleyvernon <avernon526@gmail.com>
2016-09-15 00:04:46 -0700

    changed main from seed.js to server.js so localhost will run

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -64,11 +64,13 @@ function handleRecipes(json){
   recipes.forEach(function(recipe) {
     renderRecipe(recipe);
   });
+
   var $grid = $('.grid').masonry({
     // options
     itemSelector: '.grid-item',
     columnWidth: 200
   });
+
   $grid.imagesLoaded().progress( function() {
     $grid.masonry('layout');
   });
